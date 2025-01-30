41a8aef7fb7076392cc0b6a3e1f2387c8eac4ef1
Kharoo <luka010808@gmail.com>
2025-01-31 00:18:03 +0400

    commit

---
+++ day 1/group102/day 1/lesson1.py
@@ -0,0 +1,14 @@
+name = "luka"
+
+surname = "kharadze"
+
+
+age = 16
+height = 184.5
+
+knows_programming = True
+is_ugly = False
+
+
+
+print(name + " " + str(age) + " " +str(height) +  " " + surname)
